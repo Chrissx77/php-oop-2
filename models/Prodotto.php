ee8bf2d6beb9a04a8fc82f9432759973b17549f5
ChrisGarozzo <garozzochristian@gmail.com>
2024-02-06 17:28:24 +0100

    added traits

--- models/Prodotto.php
+++ models/Prodotto.php
@@ -20,69 +20,13 @@ class Prodotto
 
 
     // SETTER METHODS
-    public function setPrezzo($prezzo)
-    {
-        $this->prezzo = $prezzo;
-    }
-
-    public function setTitolo($titolo)
-    {
-        if(!is_string($titolo) or strlen($titolo) < 3)
-            throw new Exception("Titolo is not a valid titolo");
-        $this->titolo = $titolo;
-    }
-
-    public function setIcon($icona_categoria)
-    {
-        $this->icona_categoria = $icona_categoria;
-    }
-
-    public function setTipo($tipo_articolo)
-    {
-        $this->tipo_articolo = $tipo_articolo;
-    }
-
-    public function setImmagine($immagine)
-    {
-        $this->immagine = $immagine;
-    }
-
-    public function setCategoria($categoria)
-    {
-        $this->categoria = $categoria;
-    }
+    use setterProd;
+   
 
 
     // GETTER METHODS
-    public function getPrezzo()
-    {
-        return $this->prezzo;
-    }
-
-    public function getTitolo()
-    {
-        return $this->titolo;
-    }
-
-    public function getIcon()
-    {
-        return $this->icona_categoria;
-    }
-
-    public function getTipo()
-    {
-        return $this->tipo_articolo;
-    }
-
-    public function getImmage()
-    {
-        return $this->immagine;
-    }
-
-    public function getCategoria()
-    {
-        return $this->categoria;
-    }
+    use getterProd;
+    
 
 
     public function get()
